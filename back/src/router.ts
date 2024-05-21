d48c2a60491b1fdd9be9ce4cfe8bfb2ff74ca2fb
SarahZwahlen <sarah.zwahlen@wanadoo.fr>
2024-05-21 10:37:09 +0200

    add root route test

--- back/src/router.ts
+++ back/src/router.ts
@@ -1,7 +1,10 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import sendMailController from './sendMail.controller';
 
 const router = express.Router();
+router.get('', (req: Request, res: Response) => {
+    return res.status(200).json({ message: 'Hello from Super Marmotte' });
+});
 
 router.get('/send-mail', sendMailController);
 
