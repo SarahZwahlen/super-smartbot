d02a409c87cc64a2b10d2e26955d273303de6d79
SarahZwahlen <sarah.zwahlen@wanadoo.fr>
2024-05-21 15:12:06 +0200

    WIP - fix eslint

--- back/eslint.config.js
+++ back/eslint.config.js
@@ -1,23 +1,12 @@
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = [
     {
         languageOptions: {},
         files: ['**/*.js', '**/*.ts'],
-        plugins: {
-            // prettier: prettier
-            // @typescript-eslint: @typescript-eslint
-        },
-        // extends: [
-        //     'eslint:recommended',
-        //     'plugin:@typescript-eslint/eslint-recommended',
-        //     'plugin:@typescript-eslint/recommended',
-        //     'prettier'
-        // ],
+        plugins: {},
         rules: {
             'no-console': 1
-
-            // 'prettier/prettier': 2
         }
     },
     eslintPluginPrettierRecommended
