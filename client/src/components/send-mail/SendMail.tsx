ff8c8cc5b4aee01a957ba539a980b030f76a6c3f
SarahZwahlen <sarah.zwahlen@wanadoo.fr>
2024-05-28 12:17:51 +0200

    WIP - send mail

--- client/src/components/send-mail/SendMail.tsx
+++ client/src/components/send-mail/SendMail.tsx
@@ -1,6 +1,10 @@
 export const SendMail = () => {
-  const sendMail = () => {
-    console.log("send");
+  const sendMail = async () => {
+    const response = await fetch(
+      `${process.env.REACT_APP_BACKEND_URL}/send-mail`,
+      { method: "post", body: JSON.stringify({ user: "coucou" }) }
+    );
+    console.log("response", response);
   };
 
   return <button onClick={sendMail}>Send mail</button>;
