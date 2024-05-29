a096773945f293229869054ef22686ad1b43d02b
SarahZwahlen <sarah.zwahlen@wanadoo.fr>
2024-05-29 19:21:50 +0200

    front - create http post client

---
+++ client/src/tools/httpClients/post.http.ts
@@ -0,0 +1,24 @@
+export const HTTPPostClient = async (
+  url: string,
+  body: string | FormData
+): Promise<Response> => {
+  let contentType = "";
+  if (typeof body === "string") {
+    contentType = "application/json";
+  }
+  if (body instanceof FormData) {
+    contentType = "multipart/form-data";
+  }
+
+  const request: RequestInit = {
+    method: "post",
+    body: body,
+    mode: "cors",
+    credentials: "include",
+    headers: {
+      "content-type": contentType,
+    },
+  };
+
+  return await fetch(`${process.env.REACT_APP_BACKEND_URL}/${url}`, request);
+};
