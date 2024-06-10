4ac1fdab1e80e7a34255bd99d0c8a00e2932e80e
SarahZwahlen <sarah.zwahlen@wanadoo.fr>
2024-06-10 11:52:52 +0200

    DEBUG - add backend route to show session

--- back/src/router.ts
+++ back/src/router.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import sendMailController from './useCases/sendMail/sendMail.controller';
 import { loginController } from './useCases/login/login.controller';
 import { logoutController } from './useCases/logout/logout.controller';
@@ -7,6 +7,10 @@ import { isAuthenticated } from './middlewares/auth-checker.middleware';
 
 const router = express.Router();
 router.get('', HelloController);
+router.get('/get-session', (req: Request, res: Response) => {
+    console.log('[SESSION]', req.session);
+    res.status(200).json({ message: 'OK', data: req.session });
+});
 
 router.post('/login', loginController);
 router.post('/send-mail', isAuthenticated, sendMailController);
